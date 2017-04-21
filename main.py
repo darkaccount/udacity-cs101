f3d3aa5d63a7c955573a6c444adc25852d47e68c
darkaccount <37471870@qq.com>
2017-04-21 15:21:07 +0800

    add max_pages to crawl

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 
+#!!fun just for test!!
+def get_page(url):
+    try:
+        if url == "http://www.udacity.com/cs101x/index.html":
+            return ('<html> <body> This is a test page for learning to crawl! '
+            '<p> It is a good idea to '
+            '<a href="http://www.udacity.com/cs101x/crawling.html">learn to '
+            'crawl</a> before you try to  '
+            '<a href="http://www.udacity.com/cs101x/walking.html">walk</a> '
+            'or  <a href="http://www.udacity.com/cs101x/flying.html">fly</a>. '
+            '</p> </body> </html> ')
+        elif url == "http://www.udacity.com/cs101x/crawling.html":
+            return ('<html> <body> I have not learned to crawl yet, but I '
+            'am quite good at '
+            '<a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.'
+            '</body> </html>')
+        elif url == "http://www.udacity.com/cs101x/walking.html":
+            return ('<html> <body> I cant get enough '
+            '<a href="http://www.udacity.com/cs101x/index.html">crawling</a>! '
+            '</body> </html>')
+        elif url == "http://www.udacity.com/cs101x/flying.html":
+            return ('<html> <body> The magic words are Squeamish Ossifrage! '
+            '</body> </html>')
+    except:
+        return ""
+    return ""
+
 # get next target in page 
 # return url and end_quote
 def get_next_target(page):
@@ -28,7 +55,7 @@ def get_all_links(page):
     return links                    #return links
 
 
-def crawl_web(seed):
+def crawl_web(seed, max_pages):
     tocrawl = [seed]
     crawled = []
     while tocrawl:
@@ -36,4 +63,6 @@ def crawl_web(seed):
         if page not in crawled:
             union(tocrawl, get_all_links(get_page(page)))
             crawled.append(page)
+            if len(crawled) == max_pages:
+                return crawled
     return crawled
