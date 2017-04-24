a1bab17c48a5d733c4bd62257ffb943c045a2bc9
darkaccount <37471870@qq.com>
2017-04-24 14:47:15 +0800

    add index funtions

---
+++ index.py
@@ -0,0 +1,33 @@
+def add_to_index(index, keyword, url):
+    for entry in index:
+        if entry[0] == keyword:
+            entry[1].append(url)
+            return
+    #not found, add a new entry
+    index.append([keyword,[url]])
+        
+def lookup(index, keyword):
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return []
+
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+
+def split_string(source, splitlist):
+    output = []
+    atsplit = True #At a split point
+    for char in source: #iterate through string by each letter
+        if char in splitlist:
+            atsplit = True
+        else:
+            if atsplit:
+                output.append(char)
+                atsplit = False
+            else:
+                #add character to last word
+                output[-1] = output[-1] + char
+    return output
